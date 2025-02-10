f122ccb8472c4b71532eb81e9dc4760c40275deb
Afik Yefet <afik.yefet@gmail.com>
2025-02-10 13:40:29 +0200

    axios

--- miss-bug-starter-react-vite/src/services/bug.service.js
+++ miss-bug-starter-react-vite/src/services/bug.service.js
@@ -3,9 +3,9 @@ import axios, { Axios } from 'axios'
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 
-// const axios = Axios.create({
-//     withCredentials: true
-// })
+const axios = Axios.create({
+    withCredentials: true
+})
 
 
 const STORAGE_KEY = 'bugDB'
@@ -16,6 +16,8 @@ export const bugService = {
     getById,
     save,
     remove,
+    getDefaultFilter,
+    getEmptyBug
 }
 
 
@@ -45,4 +47,8 @@ function getEmptyBug(){
         labels: [],
         severity: null,
     }
+}
+
+function getDefaultFilter(){
+    return {title: '', description: '', labels: [], severity: 0}
 }
